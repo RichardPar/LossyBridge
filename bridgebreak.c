9c301dfd9e7ecaa42a6bf92553d819f547dbda1a
Richard <tuxbox.guru@gmail.com>
2016-12-16 08:26:41 +0000

    Formatting

--- bridgebreak.c
+++ bridgebreak.c
@@ -2,7 +2,7 @@
 // NOTE : This works on ALL bridge devices
 // This code is copyright - GPL v2 License applies
 // tuxbox.guru@gmail.com
-// This code is a complete example and its hacky as heck! 
+// This code is a complete example and its hacky as heck!
 // Its prime example is how to do stuff!
 
 #ifndef __KERNEL__
@@ -29,30 +29,39 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 
-
 int lossyness;
+int jitter;
 
 int loss = 0; // Losses ... 0 to 65535 : uses a random short int to determine if the packet should be dropped
-int tot  =0;  // Total packets processes
-int drop =0;  // Total packets dropped
+int tot = 0;  // Total packets processes
+int drop = 0; // Total packets dropped
 
 module_param(loss, int, 0000);
 MODULE_PARM_DESC(bridgeDevice, "Lossyness of the bridge [0-65535]");
 
-static int loss_open(struct inode *inode, struct file *file);
-static int loss_show(struct seq_file *m, void *v);
-static ssize_t loss_write (struct file *file,const char * buf, size_t size, loff_t * ppos);
+static int loss_open(struct inode* inode, struct file* file);
+static int loss_show(struct seq_file* m, void* v);
+static ssize_t loss_write(struct file* file, const char* buf, size_t size, loff_t* ppos);
 
+static int jitter_open(struct inode* inode, struct file* file);
+static int jitter_show(struct seq_file* m, void* v);
+static ssize_t jitter_write(struct file* file, const char* buf, size_t size, loff_t* ppos);
 
-static const struct file_operations loss_fops = {
-    .owner      = THIS_MODULE,
-    .open       = loss_open,
-    .write      = loss_write,
-    .read       = seq_read,
-    .llseek     = seq_lseek,
-    .release    = single_release,
-};
+void do_jitter(void);
 
+static const struct file_operations loss_fops = { .owner = THIS_MODULE,
+                                                  .open = loss_open,
+                                                  .write = loss_write,
+                                                  .read = seq_read,
+                                                  .llseek = seq_lseek,
+                                                  .release = single_release, };
+
+static const struct file_operations jitter_fops = { .owner = THIS_MODULE,
+                                                    .open = jitter_open,
+                                                    .write = jitter_write,
+                                                    .read = seq_read,
+                                                    .llseek = seq_lseek,
+                                                    .release = single_release, };
 
 unsigned int nf_icmp_forward_hook(unsigned int hooknum,
                                   struct sk_buff* skb,
@@ -60,13 +69,11 @@ unsigned int nf_icmp_forward_hook(unsigned int hooknum,
                                   const struct net_device* out,
                                   int (*okfn)(struct sk_buff*));
 
-
 static struct nf_hook_ops nf_customtrace_ops[] __read_mostly = { { .hook = (nf_hookfn*)nf_icmp_forward_hook,
                                                                    .owner = THIS_MODULE,
                                                                    .pf = NFPROTO_BRIDGE,
                                                                    .hooknum = NF_BR_FORWARD,
-                                                                   .priority = NF_BR_PRI_FILTER_BRIDGED, },};
-                                                                 
+                                                                   .priority = NF_BR_PRI_FILTER_BRIDGED, }, };
 
 unsigned int nf_icmp_forward_hook(unsigned int hooknum,
                                   struct sk_buff* skb,
@@ -74,76 +81,136 @@ unsigned int nf_icmp_forward_hook(unsigned int hooknum,
                                   const struct net_device* out,
                                   int (*okfn)(struct sk_buff*))
 {
-   unsigned short ran;
+    unsigned short ran;
+    unsigned char interested = 0;
 
-    if (skb) {
+    if (in != NULL) {
+        if (!strncmp(in->name, "eth1", 16)) {
+            interested = 1;
+        }
+    }
+
+    if ((skb) && (interested == 1)) {
         tot++;
         get_random_bytes(&ran, 2);
-        if (ran <= loss)
-          {
+        if (ran <= loss) {
             printk("Packet Dropped\r\n");
             drop++;
-            return NF_DROP; 
-           }
-
-
+            return NF_DROP;
+        } else
+            do_jitter();
     }
     return NF_ACCEPT;
 }
 
-
-static int loss_show(struct seq_file *m, void *v)
+void do_jitter()
 {
-    seq_printf(m, "Loss : %d\nIn : %d\nDrop :%d\n", loss,tot,drop);
+    unsigned short ran;
+    unsigned short interval;
+    unsigned int actualLoop;
+    unsigned short actual;
+
+    get_random_bytes(&ran, 2);
+    interval = (0xFFFF / jitter);
+
+    actual = 0;
+
+    for (actualLoop = 0; actualLoop < ran; actualLoop += interval) {
+        actual++;
+        mdelay(1);
+    }
+    // printk("Jitter is %d\r\n",actual);
+}
+
+static int loss_show(struct seq_file* m, void* v)
+{
+    seq_printf(m, "Loss : %d\nIn : %d\nDrop :%d\n", loss, tot, drop);
     return 0;
 }
 
-static int loss_open(struct inode *inode, struct file *file)
+static int jitter_show(struct seq_file* m, void* v)
+{
+    seq_printf(m, "Jitter %dmS", jitter);
+    return 0;
+}
+
+static int loss_open(struct inode* inode, struct file* file)
 {
     return single_open(file, loss_show, NULL);
 }
 
-static ssize_t loss_write (struct file *file,const char * buf, size_t size, loff_t * ppos)
+static int jitter_open(struct inode* inode, struct file* file)
 {
- 
-  size_t len = 20;
-  char mbuf[21];
-  unsigned int val=0;
-  int err;
-
-
-  if (len > size)
-      len = size;
-
-  if (copy_from_user (mbuf, buf, len))
-    return -EFAULT;
-
-  mbuf[len] = '\0';
-
-  err = kstrtouint(mbuf, 0, &val); 
-  if (err)
-   {
-      printk("Error converting %s\r\n",mbuf);
-      return -1;
-    } else
-    {
-      loss=val;
-      tot=0;
-      drop=0;
-    }
-
-  return len;
+    return single_open(file, jitter_show, NULL);
 }
 
-int init_module() 
+static ssize_t loss_write(struct file* file, const char* buf, size_t size, loff_t* ppos)
+{
+
+    size_t len = 20;
+    char mbuf[21];
+    unsigned int val = 0;
+    int err;
+
+    if (len > size)
+        len = size;
+
+    if (copy_from_user(mbuf, buf, len))
+        return -EFAULT;
+
+    mbuf[len] = '\0';
+
+    err = kstrtouint(mbuf, 0, &val);
+    if (err) {
+        printk("Error converting %s\r\n", mbuf);
+        return -1;
+    } else {
+        loss = val;
+        tot = 0;
+        drop = 0;
+    }
+
+    return len;
+}
+
+static ssize_t jitter_write(struct file* file, const char* buf, size_t size, loff_t* ppos)
+{
+
+    size_t len = 20;
+    char mbuf[21];
+    unsigned int val = 0;
+    int err;
+
+    if (len > size)
+        len = size;
+
+    if (copy_from_user(mbuf, buf, len))
+        return -EFAULT;
+
+    mbuf[len] = '\0';
+
+    err = kstrtouint(mbuf, 0, &val);
+    if (err) {
+        printk("Error converting %s\r\n", mbuf);
+        return -1;
+    } else {
+        jitter = val + 1;
+        tot = 0;
+        drop = 0;
+    }
+
+    return len;
+}
+
+int init_module()
 {
     int ret;
-    
 
     proc_create("loss", 0, NULL, &loss_fops);
-
+    proc_create("jitter", 0, NULL, &jitter_fops);
 
     lossyness = loss;
+    jitter = 15;
 
     ret = nf_register_hooks(nf_customtrace_ops, ARRAY_SIZE(nf_customtrace_ops));
     if (ret < 0) {
@@ -156,7 +223,7 @@ int init_module()
 
 void cleanup_module()
 {
-     remove_proc_entry("loss", NULL);
+    remove_proc_entry("loss", NULL);
     nf_unregister_hooks(nf_customtrace_ops, ARRAY_SIZE(nf_customtrace_ops));
 }
 
